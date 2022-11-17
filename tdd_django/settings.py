fab6aaefc1fa5c0699cab2a9c33ef93cf321e709
Mario S J <mario2013sj@gmail.com>
2022-11-16 22:16:14 -0400

    env.example

--- tdd_django/settings.py
+++ tdd_django/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv() # loads the configs from .env 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kurgg+7!7vdi3shpb!(ieu!+=0$s#=7!@z&%)uxbf#778&gz*7'
+SECRET_KEY = str(os.getenv('SECRET_KEY')) 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'elibrary_app.apps.ElibraryAppConfig',
 ]
 
 MIDDLEWARE = [
